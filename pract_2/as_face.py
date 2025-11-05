94e3de1f08c741f3cf769fde40926f92c9ca8a1c
doshekk <rostikbondar31@gmail.com>
2025-11-05 13:03:56 +0200

    final_pract_1

--- pract_2/as_face.py
+++ pract_2/as_face.py
@@ -1,5 +1,7 @@
 from huggingface_hub import HfApi
+from huggingface_hub import list_models
 from dotenv import load_dotenv
+
 import os
 
 env_loaded = load_dotenv(dotenv_path="/media/doshek/Новый том/KI-25/Основи ген. інтел/generative-ai/pract_1/.env")
@@ -10,6 +12,11 @@ print("Значення токена", api_token)
 
 api = HfApi()
 
+models = list_models(task="text-generation", limit=5)
+print("Текстові моделі:")
+for model in models:
+    print("-", model.modelId)
+    
 try:
     user_info = api.whoami(token=api_token)
     print("API-ключ дійсний.", user_info['name'])
--- pract_2/as_openai.py
+++ pract_2/as_openai.py
@@ -2,6 +2,8 @@ from openai import OpenAI
 import os
 from dotenv import load_dotenv
 
+os.environ.pop("OPENAI_API_KEY", None)
+
 # Завантаження .env
 load_dotenv(dotenv_path="/media/doshek/Новый том/KI-25/Основи ген. інтел/generative-ai/pract_1/.env")
 api_key = os.getenv("MY_OPENAI_API_KEY")
@@ -11,9 +13,10 @@ print("🔑 API Key:", api_key)
 client = OpenAI(api_key=api_key)
 try:
     models = client.models.list()
-    print("models:")
-    for model in models.data:
-        print("-", model.id)
+    for name in models.data:
+        print(name.id)
+    
+
     response = client.chat.completions.create(
         model="gpt-4o",
         messages=[
